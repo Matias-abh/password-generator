7ee70b36ac7fb29e810a118e71642edc96f5d209
Matias-abh <matias.abh@gmail.com>
2026-04-13 18:54:14 -0300

    refactor(css): restructure CSS into modular architecture

--- js/copy-password.js
+++ js/copy-password.js
@@ -5,46 +5,46 @@ import { uiElements } from './ui.js';
 /* --------------- feedback copy ---------------*/
 
 const showCopyFeedback = (generatedPassword) => {
-    
-    const originalPlaceholder = uiElements.passwordDisplay.placeholder;
-    uiElements.passwordDisplay.value = "";
-    uiElements.passwordDisplay.placeholder = "Copied!";
-    uiElements.copyBtn.classList.add('copy-success');
-    uiElements.passwordDisplay.classList.add('copy-success-input');
-    
-    setTimeout(() => {
-        uiElements.copyBtn.classList.remove('copy-success');
-        uiElements.passwordDisplay.classList.remove('copy-success-input');
-        uiElements.passwordDisplay.value = generatedPassword;
-        uiElements.passwordDisplay.placeholder = originalPlaceholder;
-    }, 800);
+
+  const originalPlaceholder = uiElements.passwordDisplay.placeholder;
+  uiElements.passwordDisplay.value = "";
+  uiElements.passwordDisplay.placeholder = "Copied!";
+  uiElements.copyBtn.classList.add('password-generator__copy-btn--success');
+  uiElements.passwordDisplay.classList.add('password-generator__password-display--success');
+
+  setTimeout(() => {
+    uiElements.copyBtn.classList.remove('password-generator__copy-btn--success');
+    uiElements.passwordDisplay.classList.remove('password-generator__password-display--success');
+    uiElements.passwordDisplay.value = generatedPassword;
+    uiElements.passwordDisplay.placeholder = originalPlaceholder;
+  }, 800);
 };
 
 
 /* --------------- copy password ---------------*/
 
 const copyToClipboard = async (text) => {
-    try {
-        await navigator.clipboard.writeText(text);
-        return true;
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
+  try {
+    await navigator.clipboard.writeText(text);
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 };
 
 const handleCopyClick = async () => {
-    const passwordToCopy = uiElements.passwordDisplay.value;
-    if (!passwordToCopy) return;
-    const isSuccessful = await copyToClipboard(passwordToCopy);
-    if (isSuccessful) showCopyFeedback(passwordToCopy);
+  const passwordToCopy = uiElements.passwordDisplay.value;
+  if (!passwordToCopy) return;
+  const isSuccessful = await copyToClipboard(passwordToCopy);
+  if (isSuccessful) showCopyFeedback(passwordToCopy);
 
 };
 
 
 
 const initCopyPassword = () => {
-    uiElements.copyBtn.addEventListener('click', handleCopyClick);
+  uiElements.copyBtn.addEventListener('click', handleCopyClick);
 };
 
 export { copyToClipboard, initCopyPassword };
